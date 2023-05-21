ab30e349cad1fbdb26d309aac2e451ed4f6b3e1f
KariukiDavid254 <kariukidavidwangari254@gmail.com>
2023-05-21 14:00:52 +0300

    Conditional Statements

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,59 +1,44 @@
-#!/usr/bin/env python3
+# control_flow.py
 
 def admin_login(username, password):
-    # your code here
-    if username == 'admin' and password == 'admin':
-        return True
+    if username == "admin" and password == "12345":
+        return "Access granted"
     else:
-        return False
-
-    
+        return "Access denied"
 
 def hows_the_weather(temperature):
-    # your code here
-    if temperature < 10:
-    # if temperature is less than 10
-        return "it's cold"
-    elif temperature < 20:
-    # if temperature is between 10 and 20
-        return "it's mild"
-    elif temperature < 30:
-    # if temperature is between 20 and 30
-        return "it's warm"
-    elif temperature < 40:
-    # if temperature is between 30 and 40
-        return "it's hot"
-    elif temperature < 50:
-    # if temperature is between 40 and 50
-        return "it's very hot"
-    elif temperature < 60:
-    # if temperature is between 50 and 60
-        return "it's very hot"
-    
+    if temperature <= 32:
+        return "It's freezing out there!"
+    elif temperature >= 90:
+        return "It's too dang hot out there!"
+    elif temperature >= 70:
+        return "It's perfect out there!"
+    elif temperature >= 50:
+        return "It's a little chilly out there!"
+    else:
+        return "It's brisk out there"
 
 def fizzbuzz(num):
-    # your code here
     if num % 3 == 0 and num % 5 == 0:
-        return "fizzbuzz"
+        return "FizzBuzz"
     elif num % 3 == 0:
-        return "fizz"
+        return "Fizz"
     elif num % 5 == 0:
-        return "buzz"
+        return "Buzz"
     else:
         return str(num)
-    
-
 
 def calculator(operation, num1, num2):
-    # your code here
-    if operation == '+':
+    if operation == "+":
         return num1 + num2
-    elif operation == '-':
+    elif operation == "-":
         return num1 - num2
-    elif operation == '*':
+    elif operation == "*":
         return num1 * num2
-    elif operation == '/':
-        return num1 / num2
+    elif operation == "/":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Cannot divide by zero!"
     else:
-        return "invalid operation"
-    
+        return "Invalid operation!"
